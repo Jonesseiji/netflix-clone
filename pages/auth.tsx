05d3dda464287c34c1284b12c27184a7b47584d9
jonesseiji <jonesseiji9@gmail.com>
2023-04-04 05:26:45 -0300

    feat(auth): implement google and github login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,9 @@
 import { SetStateAction, useCallback, useState } from "react";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
+
 import { registerUser } from "@/services/user";
-import Input from "@/components/input";
+
 import { CREATE_ACCOUNT } from "@/constants/strings";
 import { SIGN_UP, ALREADY_HAVE_ACCOUNT } from "../constants/strings";
 import {
@@ -11,6 +12,11 @@ import {
   REGISTER,
   LOGIN,
 } from "../constants/strings";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import Input from "@/components/input";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -98,6 +104,50 @@ const Auth = () => {
             >
               {variant === "login" ? LOGIN : SIGN_UP}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() =>
+                  signIn("google", {
+                    callbackUrl: "/",
+                  })
+                }
+                className="
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  items-center
+                  justify-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+              "
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() =>
+                  signIn("github", {
+                    callbackUrl: "/",
+                  })
+                }
+                className="
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  items-center
+                  justify-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+              "
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login" ? FIRST_TIME_USING : ALREADY_HAVE_ACCOUNT}
               <span
