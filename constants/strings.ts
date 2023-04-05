2195730a92f93aeb87c9a879c292d50c7246b543
jonesseiji <jonesseiji9@gmail.com>
2023-04-05 19:54:49 -0300

    chore(strings): add strings

--- constants/strings.ts
+++ constants/strings.ts
@@ -1,3 +1,7 @@
+//#region Defaults
+export const LOGOUT = "Sign out of Netflix";
+//#endregion Defaults
+
 //#region Auth Page
 export const SIGN_IN = "Sign in";
 export const FIRST_TIME_USING = "First time using Netflix?";
@@ -6,7 +10,6 @@ export const ALREADY_HAVE_ACCOUNT = "Already have an account?";
 export const LOGIN = "Login";
 export const REGISTER = "Register";
 export const SIGN_UP = "Sign up";
-
 //#endregion Auth Page
 
 //#region API Errors strings
@@ -16,3 +19,16 @@ export const INCORRECT_PASSWORD = "Invalid password";
 export const EMAIL_ALREADY_EXISTS = "Email already exists";
 export const NOT_SIGNED_IN = "Not signed in";
 //#endregion API Errors strings
+
+//#region Mobile Menu Strings
+export const MENU_HOME = "Home";
+export const MENU_SERIES = "Series";
+export const MENU_FILMS = "Films";
+export const MENU_NEW_AND_POPULAR = "New & Popular";
+export const MENU_MY_LIST = "My List";
+export const MENU_BROWSE_BY_LANG = "Browse by Languages";
+//#endregion Mobile Menu Strings
+
+//#region Account Menu Strings
+export const ACC_MENU_USERNAME = "";
+//#endregion Account Menu Strings
