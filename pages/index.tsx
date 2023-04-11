97df1986e6ff8f79805d8fde5b6bd5ec915ddf2a
jonesseiji <jonesseiji9@gmail.com>
2023-04-11 19:37:24 -0300

    feat(pages/index): add info modal on home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
 import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 
 import BillBoard from "@/components/BillBoard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
-import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -28,9 +30,11 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <div style={{ height: "120vh" }}>
+      <InfoModal onClose={closeModal} visible={isOpen} />
       <Navbar />
       <BillBoard />
       <div className="pb-40">
