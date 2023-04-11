924f8d9e38de73ea26f4edf274f749db9e46c1f7
jonesseiji <jonesseiji9@gmail.com>
2023-04-11 19:53:45 -0300

    style(watch/movie-id): update nav styles

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -1,46 +1,64 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useRouter } from "next/router";
 import useMovie from "@/hooks/useMovie";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const Watch = () => {
+  const [showNav, setShowNav] = useState(false);
+
   const router = useRouter();
+
   const { movieId } = router.query;
   const { data } = useMovie(movieId as string);
 
+  const handleMouseEnter = () => {
+    setShowNav(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowNav(false);
+  };
+
   return (
     <div className="h-screen w-screen bg-black">
-      <nav
-        className="
-        fixed
-        w-full
-        p-4
-        z-10
-        flex
-        flex-row
-        items-center
-        gap-8
-        bg-black
-        bg-opacity-70
-        cursor-pointer
-      "
+      <div
+        className="relative"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
-        <AiOutlineArrowLeft
-          size={40}
-          className="text-white"
-          onClick={() => router.push("/")}
+        <nav
+          className={`
+            fixed
+            w-full
+            p-4
+            z-10
+            flex-row
+            items-center
+            gap-8
+            cursor-pointer
+            flex
+            ${showNav ? "opacity-100" : "opacity-0 pointer-events-none"}
+            transition-opacity duration-300 ease-in-out
+          `}
+          style={{ textShadow: "1px 2px 3px #000" }}
+        >
+          <AiOutlineArrowLeft
+            size={40}
+            className="text-white"
+            onClick={() => router.push("/")}
+          />
+          <p className="text-white text-1xl md:text-3xl font-bold">
+            <span className="mr-4 font-semibold">Watching:</span>
+            {data?.title}
+          </p>
+        </nav>
+        <video
+          src={data?.videoUrl}
+          className="h-full w-full"
+          autoPlay
+          controls
         />
-        <p className="text-white text-1xl md:text-3xl font-bold">
-          <span className="font-light">Watching: </span>
-          {data?.title}
-        </p>
-      </nav>
-      <video
-        src={data?.videoUrl}
-        className="h-full w-full"
-        autoPlay
-        controls
-      ></video>
+      </div>
     </div>
   );
 };
